9613c79bf8168b675590d52f577f5e62bd50cb4e
Piotr Fraccaro <piotr.fraccaro@iteo.com>
2021-03-07 16:17:51 +0100

    Initial Commit

---
+++ PafipiMDM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PafipiMDM
+//
+//  Created by Piotr Fraccaro on 07/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
