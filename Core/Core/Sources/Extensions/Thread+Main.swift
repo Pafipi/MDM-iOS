ef217525d7c17e8ad9cff83ede781082c86b7bf3
fraccaretti <piotr.fraccaro@gmail.com>
2021-03-21 00:46:17 +0100

    - Add Extensions group for Core module - Add Thread+Main extension

---
+++ Core/Core/Sources/Extensions/Thread+Main.swift
@@ -0,0 +1,25 @@
+//
+//  Thread+Main.swift
+//  Core
+//
+//  Created by Piotr Fraccaro on 20/03/2021.
+//
+
+import Foundation
+
+public typealias MainThreadBlock = () -> Void
+
+public extension Thread {
+
+    class func asyncOnMain(block: @escaping MainThreadBlock) {
+
+        if !Thread.isMainThread {
+            DispatchQueue.main.async {
+                block()
+            }
+        } else {
+            block()
+
+        }
+    }
+}
