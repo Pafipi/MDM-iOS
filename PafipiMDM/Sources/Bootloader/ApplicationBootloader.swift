2c18772017f6b67d0e1a2bfd217cd7a3402d5784
fraccaretti <piotr.fraccaro@gmail.com>
2021-03-21 09:55:36 +0100

    Create ApplicationBootloader class

---
+++ PafipiMDM/Sources/Bootloader/ApplicationBootloader.swift
@@ -0,0 +1,24 @@
+//
+//  ApplicationBootloader.swift
+//  PafipiMDM
+//
+//  Created by Piotr Fraccaro on 21/03/2021.
+//
+
+import Foundation
+
+/// Application bootloader
+/// Boots all services after app launch
+protocol ApplicationBootloader {
+    
+}
+
+final class ApplicationBootloaderImpl {
+    
+}
+
+// MARK: - ApplicationBootloader
+
+extension ApplicationBootloaderImpl: ApplicationBootloader {
+    
+}
